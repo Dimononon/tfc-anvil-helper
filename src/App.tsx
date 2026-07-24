77a02a5a3f3021c4666dfca001fdcbdfaf273c78
Dimononon <dimononk@gmail.com>
2026-07-24 17:22:36 +0300

    preload assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Header } from './components/Header';
 import { SolverView } from './components/SolverView';
 
+const ASSETS_TO_PRELOAD = [
+  '/assets/anvil_tp.png',
+  '/assets/anvil_tp_chevron.png',
+  '/assets/results.png',
+  '/assets/x.png',
+  '/assets/hit_light.png',
+  '/assets/hit_medium.png',
+  '/assets/hit_hard.png',
+  '/assets/punch.png',
+  '/assets/bend.png',
+  '/assets/upset.png',
+  '/assets/shrink.png',
+  '/assets/draw.png',
+  '/assets/empty_hit.png',
+  '/assets/empty_draw.png',
+  '/assets/empty_punch.png',
+  '/assets/empty_bend.png',
+  '/assets/empty_upset.png',
+  '/assets/empty_shrink.png',
+  ...Array.from({ length: 10 }, (_, i) => `/assets/${i}.png`),
+];
+
 export const App: React.FC = () => {
+  useEffect(() => {
+    ASSETS_TO_PRELOAD.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }, []);
+
   return (
     <>
       <Header />
